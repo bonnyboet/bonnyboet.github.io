277c971c3d9a7343c68690c0bd99ab7c483fabcc
bonnyboet <61854797+bonnyboet@users.noreply.github.com>
2021-03-16 22:19:24 +0900

    navigation

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 function changeCss () {
   var navElement = document.querySelector("nav");
   if(this.scrollY > window.innerHeight / (100 / 90))  {
-    navElement.style.backgroundImage = "linear-gradient(rgba(180,223, 232,0.9) 30%, rgba(0,0,0,0) 100%)";
+    navElement.style.backgroundImage = "linear-gradient(rgba(180,223, 232,0.9) 30%, rgba(255,255,255,0.0) 100%)";
     navElement.style.mixBlendMode = "luminosity";
     document.body.style.setProperty("--a-hover", "#FFFFFF");
   } else {
