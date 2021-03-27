eccdf43e2dfc08e2897d5f61763ec4a19ffe19f0
bonnyboet <61854797+bonnyboet@users.noreply.github.com>
2021-03-27 18:32:56 +0900

    Mobile Menu
    Added Mobile Menu

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 function changeCss () {
-  var navElement = document.querySelector("nav");
+  var navElement = document.querySelector("nav.desktop");
   if(this.scrollY > window.innerHeight / (100 / 90))  {
-    navElement.style.backgroundImage = "linear-gradient(rgba(180,223, 232,0.9) 30%, rgba(255,255,255,0.01) 100%)";
+    navElement.style.backgroundImage = "linear-gradient(rgba(180,223, 232,1) 20%, rgba(254,254,254,0.01) 99%)";
     navElement.style.mixBlendMode = "luminosity";
     document.body.style.setProperty("--a-hover", "#FFFFFF");
   } else {
@@ -9,4 +9,4 @@ function changeCss () {
   }
 }
 window.addEventListener("scroll", changeCss , false);
-window.addEventListener("DOMContentLoaded", changeCss)
+window.addEventListener("DOMContentLoaded", changeCss);
